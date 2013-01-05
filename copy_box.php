c8c38592c2642b5b3ad66a3c79b09b2ac8ea064d
Milamber <milamber@gmail.com>
2013-01-05 18:33:31 -0500

    upload the correct copy_box file

--- copy_box.php
+++ copy_box.php
@@ -3,25 +3,51 @@
 Name: copy_box.php
 Author: Tim Milligan
 Description: Copies specific boxes and packages from one skin to another.
-Version: 0.01
+Version: 1.0
 */
 
 // Thesis 2 skin you're trying to generate the seed for:
 $original_skin = 'thesis_classic';
-$new_skin = 'test';
+$new_skin = 'thesis_blank';
 
 // Initialize arrays
 $seed = array();
-$items = array('boxes', 'templates', 'packages', 'snippets', 'vars', 'css', 'css_custom');
+$items = array('boxes', 'packages');
 
 // Connect to WP
 define('WP_USE_THEMES', false);
 define('WP_DEBUG', false);
 require('wp-blog-header.php');
 
+echo "<pre>";
 foreach ($items as $item) {
+	echo "<h1>".$item."</h1>\n\n";
 	$seed[$item] = get_option(sprintf('%s_%s', $original_skin, $item), '');
 	//update_option(sprintf('%s_%s', $new_skin, $item), $seed[$item]);
 	print_r($seed[$item]);
 }
-echo "[$new_skin] seeded with [$original_skin] data.";
+echo "\n\n";
+foreach ($seed['boxes'] as $box) {
+	print_r($box);
+}
+echo "\n\n KEY = " . find_parent($seed['boxes'], 'TEST');
+//echo "[$new_skin] seeded with [$original_skin] data.";
+
+function find_parent($array, $needle, $parent = null) {
+    foreach ($array as $key => $value) {
+        if (is_array($value)) {
+            $pass = $parent;
+            if (is_string($key)) {
+                $pass = $key;
+            }
+            $found = find_parent($value, $needle, $pass);
+            if ($found !== false) {
+                return $found;
+            }
+        } elseif ($key === '_name' && $value === $needle) {
+            return $parent;
+        }
+    }
+
+    return false;
+}
